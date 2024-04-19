ffbbe73fdd8f661ef544492f32067aa8701dbd4f
Ahmed ABOUELLEIL <ahmed.abouelleil-sayed@epitech.eu>
2024-04-19 20:13:31 +0900

    feat(basic local search algos): Hill climbing, simulated annealing, tabu, genetic

---
+++ agents/mitsubishi.py
@@ -0,0 +1,42 @@
+from random import choice
+from time import time
+from typing import Tuple, List
+
+from action import *
+from board import GameBoard
+
+
+class Agent:  # Do not change the name of this class!
+    """
+    An agent class (Skeleton)
+    """
+      
+    # local search algorithm, simulated annealing
+    def decide_new_village(self, board: GameBoard, time_limit: float = None) -> Tuple[int, int]:
+        """
+        This algorithm search for the best place of placing a new village.
+
+        :param board: Game board to manipulate
+        :param time_limit: Timestamp for the deadline of this search.
+        :return: Tuple of coordinates
+        """
+        initial = board.get_state()
+        applicable_villages = board.get_applicable_villages()
+        current = choice(applicable_villages)
+        board.simulate_action(initial, VILLAGE(current))
+        current_fitness = board.evaluate_state()
+        T = 1
+        while time() < time_limit:
+            next_pos = choice(applicable_villages)
+            board.simulate_action(initial, VILLAGE(next_pos))
+            next_fitness = board.evaluate_state()
+            if next_fitness < current_fitness:
+                current = next_pos
+                current_fitness = next_fitness
+            else:
+                delta = next_fitness - current_fitness
+                if random() < exp(-delta/T):
+                    current = next_pos
+                    current_fitness = next_fitness
+            T *= 0.99
+        return current
---
+++ agents/twingo.py
@@ -0,0 +1,42 @@
+from random import choice
+from time import time
+from typing import Tuple, List
+
+from action import *
+from board import GameBoard
+
+
+class Agent:  # Do not change the name of this class!
+    """
+    An agent class (Skeleton)
+    """
+      
+    # local search algorithm, Tabu search with aspiration
+    def decide_new_village(self, board: GameBoard, time_limit: float = None) -> Tuple[int, int]:
+        """
+        This algorithm search for the best place of placing a new village.
+
+        :param board: Game board to manipulate
+        :param time_limit: Timestamp for the deadline of this search.
+        :return: Tuple of coordinates
+        """
+        initial = board.get_state()
+        applicable_villages = board.get_applicable_villages()
+        current = choice(applicable_villages)
+        board.simulate_action(initial, VILLAGE(current))
+        current_fitness = board.evaluate_state()
+        tabu_list = []
+        tabu_list.append(current)
+        while time() < time_limit:
+            next_pos = choice(applicable_villages)
+            while next_pos in tabu_list:
+                next_pos = choice(applicable_villages)
+            board.simulate_action(initial, VILLAGE(next_pos))
+            next_fitness = board.evaluate_state()
+            if next_fitness < current_fitness:
+                current = next_pos
+                current_fitness = next_fitness
+                tabu_list.append(current)
+                if len(tabu_list) > 5:
+                    tabu_list.pop(0)
+        return current
